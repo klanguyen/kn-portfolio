d9a1d64520c8cce72b8f48cf3ce1aee92029f3fd
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-04-28 10:14:11 -0500

    Added about me section

--- wordpress/wp-content/themes/kn-portfolio/template-parts/content-front-page.php
+++ wordpress/wp-content/themes/kn-portfolio/template-parts/content-front-page.php
@@ -1,3 +1,11 @@
-<div class="mx-auto container">
-    <p>front page content</p>
-</div>
+<section class="mx-auto container text-gray-800">
+    <div class="py-16 md:grid md:grid-cols-3 md:gap-10">
+        <img src="wp-content/themes/kn-portfolio/images/portrait.jpg" class="rounded-md mb-8" alt="Kayla portrait">
+        <div class="col-span-2">
+            <span class="bg-primary/10 p-1 rounded-md border-[1px] border-primary text-sm">About me</span>
+            <h2 class="mt-4 text-xl lg:text-3xl font-bold mb-4">Transforming visions into exceptional websites</h2>
+            <p class="lg:text-xl">Passionate and innovative Front-End Web Developer with 4+ years of experience in crafting seamless user experiences and pixel-perfect designs. Proficient in a variety of web technologies, I specialize in turning ideas into captivating and responsive websites. With a keen eye for detail and a commitment to staying updated with the latest industry trends, I thrive on creating intuitive interfaces that elevate user engagement.
+            </p>
+        </div>
+    </div>
+</section>
