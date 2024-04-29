7bc4c3170282c2ca675aec3f1c06f5248421d229
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-04-29 01:11:01 -0500

    Initial commit for Projects plugin

---
+++ wordpress/wp-content/plugins/kn-projects/kn-projects.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Portfolio Projects Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name: Portfolio Projects Plugin
+ * Description: Showcase projects in portfolio
+ * Version: 1.0.0
+ * Author: Kayla Nguyen
+ * Text Domain: kn-projects
+ */
+
+namespace KN;
+
+if(!defined('KN_PPPLUGIN_VER'))
+    define('KN_PPPLUGIN_VER', '1.0.0');
+
+class KnProjectsPlugin {
+
+}
