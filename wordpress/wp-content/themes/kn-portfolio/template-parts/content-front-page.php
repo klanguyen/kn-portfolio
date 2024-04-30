41798d84c4b8e6cc50f51df70e1641f76f117662
Kayla Nguyen <nnguyen1@my.wctc.edu>
2024-04-30 03:18:44 -0500

    Reloaded the styles

--- wordpress/wp-content/themes/kn-portfolio/template-parts/content-front-page.php
+++ wordpress/wp-content/themes/kn-portfolio/template-parts/content-front-page.php
@@ -63,6 +63,38 @@ $skills = array(
 
 <?php dynamic_sidebar( 'kn-featured-projects' ); ?>
 
+<!--<section class="mx-auto container text-gray-800 border-b-[1px] border-gray-200">
+    <div class="py-16 flex-col justify-center text-center">
+        <div class="inline-flex w-auto bg-primary/10 p-1 rounded-md border-[1px] border-primary text-sm tracking-wide">Featured Projects</div>
+        <h2 class="mt-4 text-xl lg:text-3xl font-bold mb-4">Some things I\'ve built</h2>
+        <div class="lg:grid lg:grid-cols-3 lg:gap-4 lg:justify-items-center inline-flex flex-col justify-center mb-6">
+            <div class="max-w-sm h-fit bg-white border border-gray-200 rounded-lg shadow flex flex-col justify-start text-start mb-6 lg:mb-0">
+                <a href="#">
+                    <img class="rounded-t-lg" src="https://placehold.it/500x500" alt="" />
+                </a>
+                <div class="p-5">
+                    <a href="#">
+                        <h3 class="mb-2 text-lg lg:text-2xl font-semibold text-gray-900">'.esc_html(get_the_title()).'</h3>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700">'. $briefDesc .'</p>
+                    <ul class="project-technologies mb-4">'.
+                        $technologiesOutput
+                        .'</ul>
+                    <div class="project-actions border-t-[1px] border-gray-300 pt-4">
+                        <a href="#" class="inline-flex items-center font-medium text-gray-800 hover:text-primary transition-colors duration-200">
+                            View Project
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div><a href="/projects" class="primary-outline-button">View all projects</a></div>
+    </div>
+</section>-->
+
 <?php
 $q = new WP_Query([
     'post_type' => 'testimonials',
